f83bb9846dc88573a6bbc25ffa7b0e5f1e6e3f24
Neel Smith <dnsmith.neel@gmail.com>
2021-12-06 11:45:10 -0500

    Add some notes to functions nb

--- notebooks/functions.jl
+++ notebooks/functions.jl
@@ -13,12 +13,13 @@ md"""
 
 # ╔═╡ d0c3985a-5387-11ec-1f7a-1f3d95a47e18
 md"""# Functions
-
-> TBA
 """
 
 # ╔═╡ 1a81efc8-9136-417c-a2ac-67f2fc699f15
 md"""
+TBD:
+
+
 - parameters
 - type annotations
 - default values
@@ -34,30 +35,34 @@ md"""## Error checking strings for a `USPhone`
 md"""## Error checking strings for a `BelgianPhone`
 """
 
-# ╔═╡ 25c85cc9-7f00-4262-bffb-9de5302662d5
-md"""
-Orange phone in Brussels
- +32 02 218 62 07
-
-Proximus in Louvain
-· +32 010 23 36 59
-"""
-
 # ╔═╡ 49b6abf5-5526-4b1c-a01e-339e5eb8674b
 belgianbigcities = ["02", "03", "04", "09"]
+
+# ╔═╡ 0cf7671c-bee8-4f0b-bef1-c851a7a11758
+md"""## Country for contact function
+
+
+
+"""
+
+# ╔═╡ 9d6f2cfd-7ab6-4d67-83f5-016b5fceae4e
+md"We don't know where Santa Claus really lives, but he has a US phone number."
 
 # ╔═╡ bb6cf6bf-18e9-46a3-a4a3-dfe88c49ca81
 md"""## String formatting by type"""
 
-# ╔═╡ 8f8f1619-759e-42a4-94ed-276bf75d7f0a
-md"""## Types
+# ╔═╡ cbda99c2-f77a-4395-a82e-1b22913dbcf7
+
+
+# ╔═╡ 130e14a3-6fd0-4823-bced-eb59adec1ba7
+html"""
+
+<br/><br/><br/><br/><br/>
+<blockquote>Type definitions from prior notebooks</blockquote>
 """
 
 # ╔═╡ f38dcd10-e179-485a-9d0e-77b0d3fae633
 abstract type Phone end
-
-# ╔═╡ 5897e8b4-62fc-4934-9b18-1b07ac909979
-abstract type Contact end
 
 # ╔═╡ 45b16c5a-14a1-40fd-9f66-59d112afcb23
 "10-digit US phone number"
@@ -90,6 +95,12 @@ end
 # ╔═╡ bd461172-d4b4-4eb3-a3ff-31f8aed2a4a9
 usphone("123456789c")
 
+# ╔═╡ 0041c334-ab9e-44fb-8d75-c811623c4544
+#"Country for `USPhone`"
+function country(usphone::USPhone)
+	"United States"
+end
+
 # ╔═╡ f39ab6d7-8bb9-41ed-8b16-47b7365fbed2
 """Format a `USPhone` in standard display."""
 function formattedphone(us::USPhone; international = false)
@@ -102,7 +113,7 @@ function formattedphone(us::USPhone; international = false)
 end
 
 # ╔═╡ b9982bfb-8d5d-4af8-a983-c9ec87b99883
-kris = USPhone("951",  "262", "3062")
+krisphone = USPhone("951",  "262", "3062")
 
 # ╔═╡ 10688be7-c898-4153-8687-deca2d6822f4
 "9- or 10-digit Belgian phone number"
@@ -151,6 +162,12 @@ function belgianbigcity(ph::BelgianPhone)
 
 end
 
+# ╔═╡ 39098185-3841-4438-ab2a-aad0ec36fb8c
+#"Country for `BelgianPhone`"
+function country(usphone::BelgianPhone)
+	"Belgium"
+end
+
 # ╔═╡ b8313348-e551-4023-89b6-016b4ab23045
 function formattedphone(belgian::BelgianPhone; international = false)
 	#2-555-12-12 
@@ -170,38 +187,107 @@ function formattedphone(belgian::BelgianPhone; international = false)
 end
 
 # ╔═╡ 70671756-7b02-424f-b67e-ce3632b1f768
-formattedphone(kris)
+formattedphone(krisphone)
 
 # ╔═╡ 2cd6b174-c630-4fec-a01e-b83e91a8bc2b
-formattedphone(kris, international = true)
+formattedphone(krisphone, international = true)
 
 # ╔═╡ bc29fcc5-1e70-4d6f-89a2-2f9d9489f9bd
 orangephone = BelgianPhone("0218", "6207")
 
+# ╔═╡ 5897e8b4-62fc-4934-9b18-1b07ac909979
+abstract type Contact end
+
+# ╔═╡ 3dd6a29f-3885-486c-9e27-8c41be5e8e1a
+function contactphone(contact::T) where {T <: Contact}
+	"Function `contactphone` not defined for $(typeof(contact))"
+end
+
+# ╔═╡ e2e43ec8-0a26-4b82-b93c-715686c2fd2d
+"Contact card for a person."
+struct Person <: Contact
+	lastname::AbstractString
+	firstname::AbstractString
+	phone::Phone
+end
+
+# ╔═╡ db39209c-1f6d-44a7-b1b3-d92bf0d95d9b
+function contactphone(person::Person)
+	person.phone
+end
+
+# ╔═╡ 0f5aa792-8be7-40e3-8500-29de7a88ca84
+kriscontact = Person("Kringle", "Kris", krisphone)
+
+# ╔═╡ 3c3879ac-a4ea-4032-aeec-3922daad748e
+
+
+# ╔═╡ b39b20e8-5ca4-467f-a737-540e55a99429
+"Contact card for a business"
+struct Business <: Contact
+	company::AbstractString
+	phone::Phone
+end
+
+# ╔═╡ 2030091f-598b-41b5-a73e-03e6aeee3e68
+function contactphone(business::Business)
+	business.phone
+end
+
+# ╔═╡ 349bb8cc-636f-418c-8fcf-ce3a62fc24f7
+#"Determine country of phone number for `contact`."
+function country(contact::T) where {T <: Contact}
+	contactphone(contact) |> country
+end
+
+# ╔═╡ f14cbc29-10fd-4c0a-ac97-d22f5ae13bb8
+country(kriscontact)
+
+# ╔═╡ 54bed9db-0c9e-483b-9088-dc1c9a3a8adb
+orangecontact = Business("Orange Phone, Brussels", orangephone)
+
+# ╔═╡ 4cbc35e3-db86-4234-8d7f-87ee38c72eb6
+country(orangecontact)
+
 # ╔═╡ Cell order:
 # ╟─a62c27e9-789a-4495-b7dc-229ecd0eaf18
-# ╠═d0c3985a-5387-11ec-1f7a-1f3d95a47e18
+# ╟─d0c3985a-5387-11ec-1f7a-1f3d95a47e18
 # ╠═1a81efc8-9136-417c-a2ac-67f2fc699f15
 # ╠═6c069e1b-f60d-48d3-b205-13ceded8958d
 # ╟─7baed31b-762d-498d-82f8-65d6486fbb61
-# ╠═08120255-6260-4437-921d-fbcb99c95447
+# ╟─08120255-6260-4437-921d-fbcb99c95447
 # ╠═bd461172-d4b4-4eb3-a3ff-31f8aed2a4a9
 # ╟─0b01988f-5e7b-48f3-9038-d0714b167aca
-# ╠═25c85cc9-7f00-4262-bffb-9de5302662d5
 # ╠═6eb76538-890c-4f34-aa63-6e907e78d0ac
 # ╠═49b6abf5-5526-4b1c-a01e-339e5eb8674b
 # ╠═69c1e734-dc04-412f-acd4-12c6b8d77ca7
 # ╠═3d0bf2e1-fd67-45de-a43d-6c75bab56807
 # ╠═05f7a408-f89c-42cb-8b15-ddf1d73c71bb
+# ╟─0cf7671c-bee8-4f0b-bef1-c851a7a11758
+# ╠═3dd6a29f-3885-486c-9e27-8c41be5e8e1a
+# ╠═db39209c-1f6d-44a7-b1b3-d92bf0d95d9b
+# ╠═2030091f-598b-41b5-a73e-03e6aeee3e68
+# ╠═0041c334-ab9e-44fb-8d75-c811623c4544
+# ╠═39098185-3841-4438-ab2a-aad0ec36fb8c
+# ╠═349bb8cc-636f-418c-8fcf-ce3a62fc24f7
+# ╠═54bed9db-0c9e-483b-9088-dc1c9a3a8adb
+# ╠═4cbc35e3-db86-4234-8d7f-87ee38c72eb6
+# ╠═0f5aa792-8be7-40e3-8500-29de7a88ca84
+# ╟─9d6f2cfd-7ab6-4d67-83f5-016b5fceae4e
+# ╠═f14cbc29-10fd-4c0a-ac97-d22f5ae13bb8
 # ╟─bb6cf6bf-18e9-46a3-a4a3-dfe88c49ca81
 # ╟─f39ab6d7-8bb9-41ed-8b16-47b7365fbed2
-# ╠═b8313348-e551-4023-89b6-016b4ab23045
+# ╟─b8313348-e551-4023-89b6-016b4ab23045
 # ╠═b9982bfb-8d5d-4af8-a983-c9ec87b99883
 # ╠═70671756-7b02-424f-b67e-ce3632b1f768
 # ╠═2cd6b174-c630-4fec-a01e-b83e91a8bc2b
 # ╠═bc29fcc5-1e70-4d6f-89a2-2f9d9489f9bd
-# ╟─8f8f1619-759e-42a4-94ed-276bf75d7f0a
+# ╠═cbda99c2-f77a-4395-a82e-1b22913dbcf7
+# ╟─130e14a3-6fd0-4823-bced-eb59adec1ba7
 # ╠═f38dcd10-e179-485a-9d0e-77b0d3fae633
-# ╠═5897e8b4-62fc-4934-9b18-1b07ac909979
 # ╟─45b16c5a-14a1-40fd-9f66-59d112afcb23
-# ╠═10688be7-c898-4153-8687-deca2d6822f4
+# ╟─10688be7-c898-4153-8687-deca2d6822f4
+# ╠═5897e8b4-62fc-4934-9b18-1b07ac909979
+# ╠═e2e43ec8-0a26-4b82-b93c-715686c2fd2d
+# ╠═3c3879ac-a4ea-4032-aeec-3922daad748e
+# ╠═b39b20e8-5ca4-467f-a737-540e55a99429
